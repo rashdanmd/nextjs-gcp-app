6e46a6dc8a8299c7fe8a32a2346b4b5753a5730e
rashdanmd <rashdan.developer@gmail.com>
2023-05-15 15:15:21 +0100

    initial coommit

---
+++ src/app/page.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+        <div></div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <p>By Rashdan</p>
+      </div>
+    </main>
+  );
+}
